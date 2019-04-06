adea16b72f22bb9ce65d90ffe8edddb64f39e2c4
bobbyn <20537582+bobbyn@users.noreply.github.com>
2019-04-06 23:19:14 +0300

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,9 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magento_bg_bg',
+    __DIR__
+);
